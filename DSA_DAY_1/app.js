22c187bafcec4cd001b452ce71d70fac9b7031a5
sahil shaikh <sahilshaikh49163@gmail.com>
2026-05-09 11:23:21 +0530

    Added DSA Day 1 solutions

---
+++ DSA_DAY_1/app.js
@@ -0,0 +1,175 @@
+
+
+// var : we can reassign the var and redeclare the var
+
+// console.log(name);
+
+// var name = 'sahil'
+
+// name = 'kishan'
+
+
+
+// let : we can reassign the let but we cannot redeclare the let before intilization
+
+
+// console.log(name);
+// let name = 'sahil'
+
+
+//const : const is a block scope function we cannot use berfore its initiliaztion and cannot recedeclare the function
+
+
+// const name = 'sahil'
+// console.log(name);
+
+
+//Que 1 : sum of 2 integers
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a+b);
+
+
+
+//Que 2 : realation between string and int
+
+//string 
+
+// string + string = string (concatenation)
+
+// string + int = string (concatenation)
+
+//int + int = int (arithmetic operation)
+
+
+
+//typeof
+
+
+// let s1 = 'sahil'
+// let s2 = 'khsuhi'
+
+// console.log(s1 + s2);
+
+// let s1 = 'sahil'
+// let temp = 20
+
+// console.log(typeof(s1 + temp));
+
+
+// let temp1 = 20;
+// let temp2 = 30;
+
+// console.log(typeof(temp1 + temp2));
+
+
+// let a = 20 ;
+// let b = 40;
+
+ // a is operand and + is operator 
+
+// console.log(a + b + "the sum of" + a + 'and' + b + 'is ' );
+
+//"the sum of 20" + 'and' + b + 'is ' + a + b
+
+//" the sum 0f 20 and " + b + 'is ' + a + b
+
+
+//"the sum of 20 and 40" + 'is ' + a + b
+
+// "the sum of 20 and 40 is " + a + b
+
+//"the sum of 20 and 40 is 20 "+ b
+
+//"the sum of 20 and 40 is 2040"
+
+
+//type coercion
+
+//Coercion means JavaScript automatically converts one data type into another when needed.
+
+
+// console.log('5' / 1);
+
+
+
+//que 4 :  accept and print the answer
+
+// let prompt = require('prompt-sync')()
+
+// let name = Number(prompt('enter your name :'))
+
+// console.log(typeof name);
+
+
+// console.log(`name: ${name}`);
+
+
+//que 5 swap the two number using three method
+
+//method 1 
+
+// let a = 10;
+// let b = 30;
+// console.log(a);
+// console.log(b);
+
+
+
+// let temp = a;
+//  a = b;
+//  b = temp
+
+// console.log(a);
+// console.log(b);
+
+
+//method 2 
+
+let a = 20 
+
+let b = 40
+
+console.log(a);
+console.log(b);
+
+
+
+
+// a = a + b // a = 60
+
+// b = a - b // b = 20
+
+// a = a - b // a = 40
+
+// console.log(a);
+// console.log(b);
+
+//method 3 
+
+[a,b] = [b,a]
+
+
+console.log(a);
+console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
